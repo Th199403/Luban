092ee57449fbfe0e855e2927dd42194dd6624ff8
MaShizhen <dohard@163.com>
2022-08-30 12:13:21 +0800

    Improvement: Improvement error monitoring

--- src/app/ui/App.jsx
+++ src/app/ui/App.jsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import * as Sentry from '@sentry/react';
 import { shortcutActions, priorities, ShortcutManager } from '../lib/shortcut';
 import { ToastContainer } from './components/Toast';
 import { actions as machineActions } from '../flux/machine';
@@ -126,6 +127,7 @@ class App extends PureComponent {
 
     componentDidCatch(error, errorInfo) {
         console.error('error', error, errorInfo);
+        Sentry.captureException(error, null, errorInfo);
         logErrorToGA(errorInfo);
     }
 
